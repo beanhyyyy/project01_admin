140ddcb2bc5bb01d54de24200d9cf1fa68f20a37
nguyen <quangnguyenne1411@gmail.com>
2021-06-05 22:14:21 +0700

    Update

--- admin/src/index.js
+++ admin/src/index.js
@@ -17,7 +17,7 @@
 */
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 // core components
 import Admin from "layouts/Admin.js";
@@ -28,7 +28,7 @@ ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/admin" component={Admin} />
-      <Redirect from="/" to="/admin/dashboard" />
+      <Route from="/" to="/admin/dashboard" />
     </Switch>
   </BrowserRouter>,
   document.getElementById("root")
